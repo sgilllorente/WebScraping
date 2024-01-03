3265dc5ebb04aa1175a9bbb50dbefaf36367d17d
Sergio Gil Llorente <100001940+sgilllorente@users.noreply.github.com>
2024-01-03 19:40:16 +0100

    Deleted comments

--- bot.py
+++ bot.py
@@ -16,16 +16,15 @@ def telegram_bot_sendtext(bot_message):
 while True:
     
 
-    # Comparar con el precio fijo y aplicar el umbral de cambio
+   
     current_price = get_current_price()
     print(current_price)
     
     if previous_price is not None and current_price < previous_price:
         mensaje = f"¡El precio ha bajado! Está en: {str(current_price) + '€'}\n\nEnlace: https://www.eneba.com/es/logitech-g27-like-g29-g923-g920-g25-f7a005f3"
         telegram_bot_sendtext(mensaje)
-        print(mensaje)  # Agregar un mensaje para depuración
+        print(mensaje)  
         
     previous_price = current_price   
-    # Esperar 60 segundos antes de revisar nuevamente
     time.sleep(30)
 
--- webScraping.py
+++ webScraping.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_current_price():
-    url = requests.get('https://www.eneba.com/es/logitech-g27-like-g29-g923-g920-g25-f7a005f3') #url = requests.get('https://dayspedia.com/time/countdown/?e=EfrR1ik') #
-    soup = BeautifulSoup(url.text, 'html.parser') # aquí estamos parseando la página web, es decir, analizamos el contenido del url
-    result = soup.find('span', class_ ='L5ErLT dXrfjQ') ##soup.find("span", {"class":"a-price-whole"}) 
+    url = requests.get('https://www.eneba.com/es/logitech-g27-like-g29-g923-g920-g25-f7a005f3') 
+    soup = BeautifulSoup(url.text, 'html.parser')
+    result = soup.find('span', class_ ='L5ErLT dXrfjQ') 
 
     price_text= result.text
     price_text = price_text.replace("€","")
