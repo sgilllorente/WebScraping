1443765c6876e43ac296dfb47f5e6d3b6032ca7b
Sergio Gil Llorente <100001940+sgilllorente@users.noreply.github.com>
2024-01-03 19:38:51 +0100

    Code Edited

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import time
 import requests
-from webScraping import main_price
-from webScraping import flat_price
+from webScraping import get_current_price
 
+previous_price = None
 
 def telegram_bot_sendtext(bot_message):
     bot_token = ''
@@ -17,14 +17,15 @@ while True:
     
 
     # Comparar con el precio fijo y aplicar el umbral de cambio
-    if main_price < flat_price +1:
-        mensaje = f"¡El precio ha bajado! Está en: {str(main_price) + '€'}\n\nEnlace: https://www.eneba.com/es/logitech-g27-like-g29-g923-g920-g25-f7a005f3"
+    current_price = get_current_price()
+    print(current_price)
+    
+    if previous_price is not None and current_price < previous_price:
+        mensaje = f"¡El precio ha bajado! Está en: {str(current_price) + '€'}\n\nEnlace: https://www.eneba.com/es/logitech-g27-like-g29-g923-g920-g25-f7a005f3"
         telegram_bot_sendtext(mensaje)
         print(mensaje)  # Agregar un mensaje para depuración
-        flat_price = main_price
-
         
-
+    previous_price = current_price   
     # Esperar 60 segundos antes de revisar nuevamente
     time.sleep(30)
 
--- webScraping.py
+++ webScraping.py
@@ -1,27 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+def get_current_price():
+    url = requests.get('https://www.eneba.com/es/logitech-g27-like-g29-g923-g920-g25-f7a005f3') #url = requests.get('https://dayspedia.com/time/countdown/?e=EfrR1ik') #
+    soup = BeautifulSoup(url.text, 'html.parser') # aquí estamos parseando la página web, es decir, analizamos el contenido del url
+    result = soup.find('span', class_ ='L5ErLT dXrfjQ') ##soup.find("span", {"class":"a-price-whole"}) 
 
-url = requests.get('https://www.eneba.com/es/logitech-g27-like-g29-g923-g920-g25-f7a005f3') 
-
-soup = BeautifulSoup(url.text, 'html.parser') # aquí estamos parseando la página web, es decir, analizamos el contenido del url
-
-result = soup.find('span', class_ ='L5ErLT dXrfjQ') ##soup.find("span", {"class":"a-price-whole"}) 
-
-
-price_text= result.text
-price_text = price_text.replace("€","")
-price_text = price_text.replace(",",".")
-#print(price_text)
-
-main_price = float(price_text)
-
-##condicion -> En este caso quiero que me avise el bot cuando el precio sea menor que 149.99
-flat_price = main_price +1
-
-'''
-if main_price < flat_price:
-    print("Hay oferta")
-else:
-    print("No hay oferta")
-'''
+    price_text= result.text
+    price_text = price_text.replace("€","")
+    price_text = price_text.replace(",",".")
+    
+    main_price = float(price_text)
+    return main_price
+    
